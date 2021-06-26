6af0b45f2b8a01cf33f75027d31a57f7732f46ff
Shobola Faizol <shobolafaizol@gmail.com>
2021-06-27 00:27:17 +0100

    Menu Completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import "./testimonials.scss";
 
 export default function Testimonials() {
     return (
-        <div className="testimonials" id="testimonial">
+        <div className="testimonials" id="testimonials">
             
         </div>
     )
